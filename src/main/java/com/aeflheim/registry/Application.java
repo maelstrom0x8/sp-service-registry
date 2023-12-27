1100334c1e97703e6dc68261f5710de079eff56b
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-12-27 16:59:25 +0100

    Major upgrades Upgrading Spring to 3.2.1 and to Java 21

--- src/main/java/com/aeflheim/registry/Application.java
+++ src/main/java/com/aeflheim/registry/Application.java
@@ -1,8 +1,9 @@
-package org.aeros.registry;
+package com.aeflheim.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
 @EnableEurekaServer
