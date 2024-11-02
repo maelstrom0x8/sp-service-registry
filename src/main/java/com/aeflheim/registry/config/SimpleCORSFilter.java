c138946da712e33f453ccdada9613adb4c9cfd25
Emmanuel Godwin <godel.eze@gmail.com>
2024-11-02 05:33:03 +0100

    Add CI config

---
+++ src/main/java/com/aeflheim/registry/config/SimpleCORSFilter.java
@@ -0,0 +1,24 @@
+package com.aeflheim.registry.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SimpleCORSFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, x-requested-with");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        chain.doFilter(req, res);
+    }
+}
