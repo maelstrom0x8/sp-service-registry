710b3a96177542d6058a6e1ceee13c1d8ecfecc7
Emmanuel Godwin <godel.eze@gmail.com>
2024-11-06 13:58:39 +0100

    disable refresh scope

--- src/main/java/com/aeflheim/eureka/Application.java
+++ src/main/java/com/aeflheim/eureka/Application.java
@@ -1,4 +1,4 @@
-package com.aeflheim.registry;
+package com.aeflheim.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
